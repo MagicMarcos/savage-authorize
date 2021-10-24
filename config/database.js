0efce6bd54cfb47fa9fa3bd6b3b6bbf855dc5b86
Marcos Cardoso <76229820+MagicMarcos@users.noreply.github.com>
2021-10-23 22:59:25 -0400

    test

--- config/database.js
+++ config/database.js
@@ -1,9 +1,5 @@
 // config/database.js
 module.exports = {
-	url: 'mongodb+srv://demo:demo@cluster0.q2ojb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+	url: '/*enter your db string here */',
 	dbName: 'demo',
 };
-
-// pIuybFhikT6dREN7;
-
-//mongodb+srv://savageAuth:<password>@cluster0.qbdrb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
