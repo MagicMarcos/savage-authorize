ade2034b79c07da9b008bf762a35960e475f9f0b
Marcos Cardoso <76229820+MagicMarcos@users.noreply.github.com>
2021-10-25 11:28:50 -0400

    db update

--- config/database.js
+++ config/database.js
@@ -1,5 +1,7 @@
 // config/database.js
 module.exports = {
-	url: 'mongodb+srv://demo:demo@cluster0.q2ojb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+	url: 'mongodb+srv://savageAuth:w5bzW6ZlKTj4KAvp@cluster0.qbdrb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
 	dbName: 'demo',
 };
+
+// w5bzW6ZlKTj4KAvp;
